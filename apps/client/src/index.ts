3abfb8688bc25e95748f495302c2155f6e064118
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-05-11 13:02:51 +0100

    add vite, update TS to beta

---
+++ apps/client/src/index.ts
@@ -0,0 +1,7 @@
+import { test } from "@/aliastest.js";
+
+const body = document.querySelector("body");
+
+if (body) {
+  body.innerText = test;
+}
---
+++ apps/client/vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
