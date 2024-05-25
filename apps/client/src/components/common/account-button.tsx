a67ff83aa1a91a6556e328e193a6b7ba7731cf74
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-05-25 17:55:01 +0100

    add logout option with confirm modal

--- apps/client/src/components/common/account-button.tsx
+++ apps/client/src/components/common/account-button.tsx
@@ -10,38 +10,66 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "../atom/dropdown-menu.js";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "../atom/dialog.js";
 
-export function AccountButton(): ReactElement {
+export function AccountDropdownMenu(): ReactElement {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size="icon" variant="outline">
-          <UserRound />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Account</DropdownMenuLabel>
-        <DropdownMenuItem asChild>
-          <Link to="/account/login" preload="intent">
-            <LogIn className="mr-2 h-4 w-4 inline" />
-            <span>Log in</span>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link to="/account/signup" preload="intent">
-            <UserPlus className="mr-2 h-4 w-4 inline" />
-            <span>Sign up</span>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuItem asChild>
-          <Link to="/upload" preload="intent">
-            <Upload className="mr-2 h-4 w-4 inline" />
-            <span>Upload</span>
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="outline">
+            <UserRound />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Account</DropdownMenuLabel>
+          <DropdownMenuItem asChild>
+            <Link to="/account/login" preload="intent">
+              <LogIn className="mr-2 h-4 w-4 inline" />
+              <span>Log in</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link to="/account/signup" preload="intent">
+              <UserPlus className="mr-2 h-4 w-4 inline" />
+              <span>Sign up</span>
+            </Link>
+          </DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <a>
+                <UserPlus className="mr-2 h-4 w-4 inline" />
+                <span>Log out</span>
+              </a>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem asChild>
+            <Link to="/upload" preload="intent">
+              <Upload className="mr-2 h-4 w-4 inline" />
+              <span>Upload</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Are you sure?</DialogTitle>
+          <DialogDescription>You are about to log out.</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="submit">Log out</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- apps/client/src/components/common/header.tsx
+++ apps/client/src/components/common/header.tsx
@@ -3,7 +3,7 @@ import { type ReactElement } from "react";
 import { ToggleImageFit } from "./toggle-image-fit.js";
 import { DarkModeToggle } from "./dark-mode-toggle.js";
 import { Link } from "@tanstack/react-router";
-import { AccountButton } from "./account-button.js";
+import { AccountDropdownMenu } from "./account-button.js";
 
 export function Header(): ReactElement {
   return (
@@ -16,7 +16,7 @@ export function Header(): ReactElement {
       <div className="shrink flex items-center gap-4">
         Contain images? <ToggleImageFit />
         <DarkModeToggle />
-        <AccountButton />
+        <AccountDropdownMenu />
       </div>
     </div>
   );
